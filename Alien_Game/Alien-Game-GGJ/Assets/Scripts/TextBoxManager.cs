8c478ca9d2a0d0a6ce10156cf5e25bc8101445e4
LiefTheBeef <32721304+LiefTheBeef@users.noreply.github.com>
2019-01-27 14:08:29 -0800

    TextboxManager

---
+++ Alien_Game/Alien-Game-GGJ/Assets/Scripts/TextBoxManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class TextBoxManager : MonoBehaviour
+{
+    public GameObject textBox;
+    public Text text;
+    public TextAsset textFile;
+    public Rigidbody2D player;
+    
+    public string[] textLines;
+    public int currentLine;
+    public int endAtLine;
+    
+    void Start()
+    {
+        player.velocity = Vector2.zero;
+        
+        if (textFile != null)
+        {
+            textLines = textFile.text.Split(('~'));
+        }
+
+        if (endAtLine == 0)
+        {
+            endAtLine = textLines.Length - 1;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        text.text = textLines[currentLine];
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            currentLine += 1;
+        }
+
+        if (currentLine > endAtLine)
+        {
+            textBox.SetActive(false);
+        }
+
+        if (currentLine != endAtLine)
+        {
+            player.velocity = Vector2.zero;
+        }
+    }
+}
